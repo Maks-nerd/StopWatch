3427b8123f7c21018c6c011bac6fbc6736af27f8
Maksym Kalishenko <maksim.kalishenko.nerd@gmail.com>
2021-12-08 09:23:11 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+  const [btnText, setbtnText] = useState('');
+  const [wait, setWait] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if(start){
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10);
+      setbtnText('Stop')
+      setWait(false);
+    } else {
+      clearInterval(interval);
+      setbtnText('Start')
+      setWait(true);
+    }
+
+    return () => clearInterval(interval);
+  }, [start]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>StopWatch</h1>
+      <h1>
+        <span className="me-3">{('0' + Math.floor(( time / 60000 ) % 60)).slice(-2)}</span>
+        <span className="me-3">{('0' + Math.floor(( time / 1000 ) % 60)).slice(-2)}</span>
+        <span>{('0' + ( time / 10 ) % 1000).slice(-2)}</span>
+      </h1>
+      <div>
+        <button type="button" className="me-3" onClick={() => {!wait && setTime(0); start ? setStart(false) : setStart(true);}}>{btnText}</button>
+        <button type="button" className="me-3" onDoubleClick={() => {setStart(false); setWait(true);}}>Wait</button>
+        {/* <button type="button" className="me-3" onClick={clickHandler}>Wait</button> */}
+        <button type="button" onClick={() => {setTime(0); setStart(true); setWait(false);}}>Reset</button>
+      </div>
     </div>
   );
 }
